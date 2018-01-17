1e85e87ea9f352732e8cd5ceb8c32b0507c9b3df
范利峰 <362517415@qq.com>
2018-01-17 16:01:09 +0800

    appicon和开屏页更新

---
+++ EdsPay/Configuration(配置)/AppColor.swift
@@ -0,0 +1,19 @@
+//
+//  AppColor.swift
+//  EdsPay
+//
+//  Created by 范利峰 on 2018/1/17.
+//  Copyright © 2018年 com.chuxiang. All rights reserved.
+//
+
+import UIKit
+ /************统一配置app的颜色**********************/
+ let AppMainColor = UIColor.hexStringWithColor(hexString: "#ff5b4c")//app主题色
+ let ViewBackGroundColor = UIColor.hexStringWithColor(hexString: "f8f8f8")//view背景色
+let TableView_Cell_BackgroundColor = UIColor.init(red: 0.95, green: 0.95, blue: 0.95, alpha: 0);//cell的背景色
+let cell_SeparatorColor = UIColor.hexStringWithColor(hexString: "#dddddd")//分割线的背景色
+
+
+class AppColor: NSObject {
+    
+}
